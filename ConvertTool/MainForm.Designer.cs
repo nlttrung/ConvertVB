297d429af5b9e806e51c0abc385dd9bcbaa376ee
ttrung2111 <ttrung2111@gmail.com>
2019-11-10 23:22:36 +0700

    update

--- ConvertTool/MainForm.Designer.cs
+++ ConvertTool/MainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txt_inputFile = new System.Windows.Forms.TextBox();
             this.btnBrowse = new System.Windows.Forms.Button();
@@ -39,23 +39,29 @@
             this.label3 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.txtComment_END = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_replace = new System.Windows.Forms.TextBox();
+            this.btnReplace1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnConvert
+            // btnReplace
             // 
-            this.btnConvert.Location = new System.Drawing.Point(251, 217);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(164, 23);
-            this.btnConvert.TabIndex = 2;
-            this.btnConvert.Text = "Convert";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            this.btnReplace.Location = new System.Drawing.Point(245, 112);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(164, 23);
+            this.btnReplace.TabIndex = 2;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // label1
             // 
@@ -101,15 +107,15 @@
             // 
             // txtComment_STR
             // 
-            this.txtComment_STR.Location = new System.Drawing.Point(86, 138);
+            this.txtComment_STR.Location = new System.Drawing.Point(73, 85);
             this.txtComment_STR.Name = "txtComment_STR";
-            this.txtComment_STR.Size = new System.Drawing.Size(528, 20);
+            this.txtComment_STR.Size = new System.Drawing.Size(545, 20);
             this.txtComment_STR.TabIndex = 7;
             // 
             // chkComment
             // 
             this.chkComment.AutoSize = true;
-            this.chkComment.Location = new System.Drawing.Point(29, 115);
+            this.chkComment.Location = new System.Drawing.Point(16, 62);
             this.chkComment.Name = "chkComment";
             this.chkComment.Size = new System.Drawing.Size(173, 17);
             this.chkComment.TabIndex = 9;
@@ -130,69 +136,123 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 66);
+            this.tabControl1.Location = new System.Drawing.Point(0, 184);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(690, 428);
+            this.tabControl1.Size = new System.Drawing.Size(690, 395);
             this.tabControl1.TabIndex = 11;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.txtComment_END);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.txtRule);
-            this.tabPage1.Controls.Add(this.chkComment);
-            this.tabPage1.Controls.Add(this.txtComment_STR);
-            this.tabPage1.Controls.Add(this.btnConvert);
+            this.tabPage1.Controls.Add(this.btnReplace);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(682, 402);
+            this.tabPage1.Size = new System.Drawing.Size(682, 313);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Replace Theo Rule";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // txtComment_END
+            // 
+            this.txtComment_END.Location = new System.Drawing.Point(73, 111);
+            this.txtComment_END.Name = "txtComment_END";
+            this.txtComment_END.Size = new System.Drawing.Size(545, 20);
+            this.txtComment_END.TabIndex = 11;
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.btnReplace1);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.txt_replace);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.txt_search);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(682, 435);
+            this.tabPage2.Size = new System.Drawing.Size(682, 369);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Replace Thường";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // txtComment_END
-            // 
-            this.txtComment_END.Location = new System.Drawing.Point(86, 164);
-            this.txtComment_END.Name = "txtComment_END";
-            this.txtComment_END.Size = new System.Drawing.Size(528, 20);
-            this.txtComment_END.TabIndex = 11;
-            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.txtComment_END);
             this.panel1.Controls.Add(this.txt_inputFile);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.chkComment);
+            this.panel1.Controls.Add(this.txtComment_STR);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(690, 66);
+            this.panel1.Size = new System.Drawing.Size(690, 184);
             this.panel1.TabIndex = 12;
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(34, 31);
+            this.txt_search.Multiline = true;
+            this.txt_search.Name = "txt_search";
+            this.txt_search.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_search.Size = new System.Drawing.Size(605, 120);
+            this.txt_search.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Search";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Replace";
+            // 
+            // txt_replace
+            // 
+            this.txt_replace.Location = new System.Drawing.Point(34, 185);
+            this.txt_replace.Multiline = true;
+            this.txt_replace.Name = "txt_replace";
+            this.txt_replace.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_replace.Size = new System.Drawing.Size(605, 131);
+            this.txt_replace.TabIndex = 8;
+            // 
+            // btnReplace1
+            // 
+            this.btnReplace1.Location = new System.Drawing.Point(253, 326);
+            this.btnReplace1.Name = "btnReplace1";
+            this.btnReplace1.Size = new System.Drawing.Size(164, 23);
+            this.btnReplace1.TabIndex = 10;
+            this.btnReplace1.Text = "Replace";
+            this.btnReplace1.UseVisualStyleBackColor = true;
+            this.btnReplace1.Click += new System.EventHandler(this.btnReplace1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 494);
+            this.ClientSize = new System.Drawing.Size(690, 579);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Convert Code";
+            this.Text = "Replace Code";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -200,7 +260,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnReplace;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_inputFile;
         private System.Windows.Forms.Button btnBrowse;
@@ -214,6 +274,11 @@
         private System.Windows.Forms.TextBox txtComment_END;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReplace1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_replace;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_search;
     }
 }
 
--- ConvertTool/MainForm.cs
+++ ConvertTool/MainForm.cs
@@ -70,18 +70,41 @@ namespace ConvertTool
         /// <param name="e"></param>
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            if (!this.checkInput()) return;
+            String inputFileNm = txt_inputFile.Text;
+            if (!File.Exists(inputFileNm))
+            {
+                MessageBox.Show("File không tồn tại", "Error", MessageBoxButtons.OK);
+                return;
+            }
 
-            CodeReplace tool = new CodeReplace();
-            string rule = txtRule.Text;
+            if (chkComment.Checked && String.IsNullOrEmpty(txtComment_STR.Text) && String.IsNullOrEmpty(txtComment_END.Text))
+            {
+                MessageBox.Show("Hãy nhập nội dung comment", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtRule.Text))
+            {
+                MessageBox.Show("Vui lòng nhập rule", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (!txtRule.Text.Contains("@"))
+            {
+                MessageBox.Show("Rule phải có ký tự @ để xác định chuyển đổi", "Error", MessageBoxButtons.OK);
+                return;
+            }
 
             File.Move(txt_inputFile.Text, txt_inputFile.Text + "_tmp");
 
             // Read the file and display it line by line.  
-            string line;
             System.IO.StreamReader file = new System.IO.StreamReader(txt_inputFile.Text + "_tmp");
             System.IO.StreamWriter wfile = File.CreateText(txt_inputFile.Text);
 
+            CodeReplace tool = new CodeReplace();
+            string rule = txtRule.Text;
+
+            string line;
             while ((line = file.ReadLine()) != null)
             {
                 bool hasChanged = false;
@@ -101,41 +124,92 @@ namespace ConvertTool
             file.Close();
             wfile.Close();
             MessageBox.Show("OK");
-        } 
+        }
         #endregion
 
-        #region private Boolean checkInput()
+        #region private void btnReplace1_Click(object sender, EventArgs e)
         /// <summary>
         /// 
         /// </summary>
-        /// <returns></returns>
-        private Boolean checkInput()
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReplace1_Click(object sender, EventArgs e)
         {
             String inputFileNm = txt_inputFile.Text;
             if (!File.Exists(inputFileNm))
             {
                 MessageBox.Show("File không tồn tại", "Error", MessageBoxButtons.OK);
-                return false;
+                return;
             }
 
-            if (String.IsNullOrEmpty(txtRule.Text))
-            {
-                MessageBox.Show("Vui lòng nhập rule", "Error", MessageBoxButtons.OK);
-                return false;
-            }
-
-            if (!txtRule.Text.Contains("@"))
-            {
-                MessageBox.Show("Rule phải có ký tự @ để xác định chuyển đổi", "Error", MessageBoxButtons.OK);
-                return false;
-            }
-
-            if(chkComment.Checked && String.IsNullOrEmpty(txtComment_STR.Text) && String.IsNullOrEmpty(txtComment_END.Text))
+            if (chkComment.Checked && String.IsNullOrEmpty(txtComment_STR.Text) && String.IsNullOrEmpty(txtComment_END.Text))
             {
                 MessageBox.Show("Hãy nhập nội dung comment", "Error", MessageBoxButtons.OK);
-                return false;
+                return;
             }
-            return true;
+
+            if (String.IsNullOrEmpty(txt_search.Text))
+            {
+                MessageBox.Show("Hãy text cần tìm kiếm", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            //input search text
+            String oldCode = txt_search.Text;
+            String newCode = Environment.NewLine + txt_replace.Text + Environment.NewLine;
+            String[] arrInput = oldCode.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            List<String> arrSearch = new List<string>();
+            foreach(String i in arrInput)
+            {
+                string rawLine = i.Trim(new char[] { ' ', '\t' });
+                if (!String.IsNullOrEmpty(rawLine)) arrSearch.Add(rawLine);
+            }
+
+            File.Move(txt_inputFile.Text, txt_inputFile.Text + "_tmp");
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(txt_inputFile.Text + "_tmp");
+            System.IO.StreamWriter wfile = File.CreateText(txt_inputFile.Text);
+
+            List<String> tmpCode = new List<string>();
+            List<String> compareCode = new List<string>();
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string rawLine = line.Trim(new char[] { ' ', '\t' });
+                if (arrSearch.Contains(rawLine))
+                {
+                    tmpCode.Add(line);
+                    if (!String.IsNullOrEmpty(rawLine)) compareCode.Add(rawLine);
+                    if(compareCode.Count == arrSearch.Count)
+                    {
+                        if (chkComment.Checked) wfile.WriteLine(txtComment_STR.Text);
+                        foreach (string cd in tmpCode) wfile.WriteLine("'" + cd);
+                        wfile.Write(newCode);
+                        if (chkComment.Checked) wfile.WriteLine(txtComment_END.Text);
+
+                        tmpCode.Clear();
+                        compareCode.Clear();
+                    }
+                }
+                else
+                {
+                    if (String.IsNullOrEmpty(rawLine))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        foreach (string cd in tmpCode) wfile.WriteLine(cd);
+                        tmpCode.Clear();
+                        compareCode.Clear();
+                        wfile.WriteLine(line);
+                    }
+                }
+            }
+            file.Close();
+            wfile.Close();
+            MessageBox.Show("OK");
         } 
         #endregion
     }
